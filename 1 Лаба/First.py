79144081cbe68892f2faef9831cabf13fe63f9be
swmenz <cyclopss87@mail.ru>
2025-05-29 23:36:36 +0600

    Update First.py

--- 1 Лаба/First.py
+++ 1 Лаба/First.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_drom_ads():
-    url = 'https://auto.drom.ru/'  # Главная страница раздела автомобилей
+    url = 'https://auto.drom.ru/'  # Главная страница авто
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:110.0) Gecko/20100101 Firefox/110.0'
     }
